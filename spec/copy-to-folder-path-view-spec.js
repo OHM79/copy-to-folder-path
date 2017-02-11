b278abc3b932adb1cb6a12f4cc60f57b2eb7ed93
OHM79 <ruin_of_messnger@yahoo.co.jp>
2017-02-11 18:53:38 +0900

    gif image追加

--- spec/copy-to-folder-path-view-spec.js
+++
@@ -1,9 +0,0 @@
-'use babel';
-
-import CopyToFolderPathView from '../lib/copy-to-folder-path-view';
-
-describe('CopyToFolderPathView', () => {
-  it('has one valid test', () => {
-    expect('life').toBe('easy');
-  });
-});
